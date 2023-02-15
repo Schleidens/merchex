589acc2b5803b7c3c3bf18f5034d80e44eacee27
Schleidens <alectineschleidens2016@gmail.com>
2023-02-15 13:21:10 -0500

    added 2 view and set urls end of view chapter

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def hello(request):
+    return HttpResponse('<h1>Im new to Django</h1>')
+
+def homePage(request):
+    return HttpResponse("<h1>I'm the HomePage</h1>")    
--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from listings import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.homePage),
+    path("hello/", views.hello)
 ]
