9e5818e39ae8b391011ab88045b37343696f96f4
Schleidens <alectineschleidens2016@gmail.com>
2023-02-15 14:17:27 -0500

    print data from Band model in Homepage

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Band
 
 # Create your views here.
 
@@ -7,4 +8,13 @@ def hello(request):
     return HttpResponse('<h1>Im new to Django</h1>')
 
 def homePage(request):
-    return HttpResponse("<h1>I'm the HomePage</h1>")    
+    bands = Band.objects.all()
+    return HttpResponse(f"""
+    <h1>I'm the HomePage</h1>
+    <br />
+    <h2>Here a list pf group</h2>
+    <ul>
+    <li>{bands[0].name}</li>
+    <li>{bands[1].name}</li>
+    </ul>
+    """)    
