ce49c3f291348750103267aab361044cdd9dd8e7
Schleidens <alectineschleidens2016@gmail.com>
2023-02-16 12:33:41 -0500

    custom schema with models and add data in the using the shell / validate and more

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,9 +1,23 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
 class Band(models.Model):
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+
     name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=50)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(
+        validators=[MaxValueValidator(1900), MinValueValidator(2023)]
+    )
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
 
 class Resources(models.Model):
     title = models.fields.CharField(max_length=100)
+    link = models.fields.URLField(null=True, blank=True)
