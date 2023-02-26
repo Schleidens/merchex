12538c013f7b57577e9d21f583c7412f238f0254
Schleidens <alectineschleidens2016@gmail.com>
2023-02-25 22:45:12 -0500

    add name to resources url pattern

--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -20,7 +20,7 @@ from listings import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homePage, name="homePage"),
-    path("resources/", views.resources),
+    path("resources/", views.resources, name="resources"),
     path('band/<int:id>', views.band_details, name="band-view"),
     path('contact/', views.contact_form, name='contact')
 ]
