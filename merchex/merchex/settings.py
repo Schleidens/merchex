bb4ef95f8997f323ef4917e17b0fa7034eb02897
Schleidens <alectineschleidens2016@gmail.com>
2023-03-12 23:13:45 -0400

    fixing debug typo and remove unnecessary code

--- merchex/merchex/settings.py
+++ merchex/merchex/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0%lw4&(4ow*nxpzzturr=357)0th^6l=p+a@-+@05lowpd!s+h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
